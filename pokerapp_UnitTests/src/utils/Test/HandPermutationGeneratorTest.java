a4186895894ff36fc8a1786c404991ae66fadbab
Sam Keays <samuelkeays@hotmail.com>
2013-02-24 18:29:43 +0000

    Hand permutator and unit test

--- pokerapp_UnitTests/src/utils/Test/HandPermutationGeneratorTest.java
+++ pokerapp_UnitTests/src/utils/Test/HandPermutationGeneratorTest.java
@@ -1,5 +1,18 @@
 package utils.Test;
 
+import org.junit.Test;
+import pokerapp.Hand;
+import pokerapp.Card;
+import pokerapp.HandFactory;
+import utils.HandPermutationGenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static junit.framework.Assert.assertTrue;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Samuel Keays
@@ -8,4 +21,147 @@ package utils.Test;
  * To change this template use File | Settings | File Templates.
  */
 public class HandPermutationGeneratorTest {
+
+    static private Set<String> PERMUTATION_TEST_VALUES  = new HashSet<String>(Arrays.asList(new String[]{"D9 S4 C1 H3 C5 ",
+            "D9 S4 C1 C5 H3 ",
+            "D9 S4 H3 C1 C5 ",
+            "D9 S4 H3 C5 C1 ",
+            "D9 S4 C5 H3 C1 ",
+            "D9 S4 C5 C1 H3 ",
+            "D9 C1 S4 H3 C5 ",
+            "D9 C1 S4 C5 H3 ",
+            "D9 C1 H3 S4 C5 ",
+            "D9 C1 H3 C5 S4 ",
+            "D9 C1 C5 H3 S4 ",
+            "D9 C1 C5 S4 H3 ",
+            "D9 H3 C1 S4 C5 ",
+            "D9 H3 C1 C5 S4 ",
+            "D9 H3 S4 C1 C5 ",
+            "D9 H3 S4 C5 C1 ",
+            "D9 H3 C5 S4 C1 ",
+            "D9 H3 C5 C1 S4 ",
+            "D9 C5 C1 H3 S4 ",
+            "D9 C5 C1 S4 H3 ",
+            "D9 C5 H3 C1 S4 ",
+            "D9 C5 H3 S4 C1 ",
+            "D9 C5 S4 H3 C1 ",
+            "D9 C5 S4 C1 H3 ",
+            "S4 D9 C1 H3 C5 ",
+            "S4 D9 C1 C5 H3 ",
+            "S4 D9 H3 C1 C5 ",
+            "S4 D9 H3 C5 C1 ",
+            "S4 D9 C5 H3 C1 ",
+            "S4 D9 C5 C1 H3 ",
+            "S4 C1 D9 H3 C5 ",
+            "S4 C1 D9 C5 H3 ",
+            "S4 C1 H3 D9 C5 ",
+            "S4 C1 H3 C5 D9 ",
+            "S4 C1 C5 H3 D9 ",
+            "S4 C1 C5 D9 H3 ",
+            "S4 H3 C1 D9 C5 ",
+            "S4 H3 C1 C5 D9 ",
+            "S4 H3 D9 C1 C5 ",
+            "S4 H3 D9 C5 C1 ",
+            "S4 H3 C5 D9 C1 ",
+            "S4 H3 C5 C1 D9 ",
+            "S4 C5 C1 H3 D9 ",
+            "S4 C5 C1 D9 H3 ",
+            "S4 C5 H3 C1 D9 ",
+            "S4 C5 H3 D9 C1 ",
+            "S4 C5 D9 H3 C1 ",
+            "S4 C5 D9 C1 H3 ",
+            "C1 S4 D9 H3 C5 ",
+            "C1 S4 D9 C5 H3 ",
+            "C1 S4 H3 D9 C5 ",
+            "C1 S4 H3 C5 D9 ",
+            "C1 S4 C5 H3 D9 ",
+            "C1 S4 C5 D9 H3 ",
+            "C1 D9 S4 H3 C5 ",
+            "C1 D9 S4 C5 H3 ",
+            "C1 D9 H3 S4 C5 ",
+            "C1 D9 H3 C5 S4 ",
+            "C1 D9 C5 H3 S4 ",
+            "C1 D9 C5 S4 H3 ",
+            "C1 H3 D9 S4 C5 ",
+            "C1 H3 D9 C5 S4 ",
+            "C1 H3 S4 D9 C5 ",
+            "C1 H3 S4 C5 D9 ",
+            "C1 H3 C5 S4 D9 ",
+            "C1 H3 C5 D9 S4 ",
+            "C1 C5 D9 H3 S4 ",
+            "C1 C5 D9 S4 H3 ",
+            "C1 C5 H3 D9 S4 ",
+            "C1 C5 H3 S4 D9 ",
+            "C1 C5 S4 H3 D9 ",
+            "C1 C5 S4 D9 H3 ",
+            "H3 S4 C1 D9 C5 ",
+            "H3 S4 C1 C5 D9 ",
+            "H3 S4 D9 C1 C5 ",
+            "H3 S4 D9 C5 C1 ",
+            "H3 S4 C5 D9 C1 ",
+            "H3 S4 C5 C1 D9 ",
+            "H3 C1 S4 D9 C5 ",
+            "H3 C1 S4 C5 D9 ",
+            "H3 C1 D9 S4 C5 ",
+            "H3 C1 D9 C5 S4 ",
+            "H3 C1 C5 D9 S4 ",
+            "H3 C1 C5 S4 D9 ",
+            "H3 D9 C1 S4 C5 ",
+            "H3 D9 C1 C5 S4 ",
+            "H3 D9 S4 C1 C5 ",
+            "H3 D9 S4 C5 C1 ",
+            "H3 D9 C5 S4 C1 ",
+            "H3 D9 C5 C1 S4 ",
+            "H3 C5 C1 D9 S4 ",
+            "H3 C5 C1 S4 D9 ",
+            "H3 C5 D9 C1 S4 ",
+            "H3 C5 D9 S4 C1 ",
+            "H3 C5 S4 D9 C1 ",
+            "H3 C5 S4 C1 D9 ",
+            "C5 S4 C1 H3 D9 ",
+            "C5 S4 C1 D9 H3 ",
+            "C5 S4 H3 C1 D9 ",
+            "C5 S4 H3 D9 C1 ",
+            "C5 S4 D9 H3 C1 ",
+            "C5 S4 D9 C1 H3 ",
+            "C5 C1 S4 H3 D9 ",
+            "C5 C1 S4 D9 H3 ",
+            "C5 C1 H3 S4 D9 ",
+            "C5 C1 H3 D9 S4 ",
+            "C5 C1 D9 H3 S4 ",
+            "C5 C1 D9 S4 H3 ",
+            "C5 H3 C1 S4 D9 ",
+            "C5 H3 C1 D9 S4 ",
+            "C5 H3 S4 C1 D9 ",
+            "C5 H3 S4 D9 C1 ",
+            "C5 H3 D9 S4 C1 ",
+            "C5 H3 D9 C1 S4 ",
+            "C5 D9 C1 H3 S4 ",
+            "C5 D9 C1 S4 H3 ",
+            "C5 D9 H3 C1 S4 ",
+            "C5 D9 H3 S4 C1 ",
+            "C5 D9 S4 H3 C1 ",
+            "C5 D9 S4 C1 H3 "}));
+
+    static int FIVE_FACTORIAL = 120;
+
+    @Test
+    public void correctPermutationsOutputted()
+    {
+
+
+        HandFactory testHandFactory = new HandFactory();
+        String[] testHandString = {"D9", "S4", "C1", "H3", "C5"};
+        try {
+            Hand testHand = testHandFactory.create(testHandString);
+            ArrayList<Hand> results = HandPermutationGenerator.permute(testHand);
+            for(Hand result : results)
+            {
+                assertTrue(PERMUTATION_TEST_VALUES.contains(result.toUserString()));
+            }
+            assertTrue(results.size() == FIVE_FACTORIAL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
