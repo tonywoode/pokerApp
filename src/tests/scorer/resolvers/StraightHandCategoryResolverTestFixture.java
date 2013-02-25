0c928440ecfe5479f68c52bdd32bd029a32f1966
ar1g <ari.lounging+GH@gmail.com>
2013-02-25 23:49:28 +0000

    StraightHandCategoryResolverTestFixture: test stub

---
+++ src/tests/scorer/resolvers/StraightHandCategoryResolverTestFixture.java
@@ -0,0 +1,41 @@
+package tests.scorer.resolvers;
+
+import org.junit.Test;
+import pokerapp.scorer.categories.HandCategory;
+import pokerapp.scorer.resolvers.StraightHandCategoryResolver;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ari G
+ * Date: 25/02/13
+ * Time: 21:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class StraightHandCategoryResolverTestFixture extends HandCategoryResolverTestFixtureBase {
+     //TODO: this is just a stub, need to understand the (elegant) class structure
+    //TODO: could perhaps use Sam's permutator or variation thereof to populate test cases
+    public StraightHandCategoryResolverTestFixture(){
+        super(new StraightHandCategoryResolver());
+    }
+
+    @Test
+    public void Hand_Is_Straight_in_order() {
+        HandCategory category = resolveHand("D2", "S3", "C4", "D5", "H6");
+
+        assertEquals("Is straight", "straight", category.getName());
+    }
+    @Test
+    public void Hand_Is_Straight_not_in_order() {
+        HandCategory category = resolveHand("D3", "S5", "C2", "D4", "H6");
+
+        assertEquals("Is straight", "straight", category.getName());
+    }
+    @Test
+    public void Hand_Is_Not_Straight() {
+        HandCategory category = resolveHand("D3", "S9", "C2", "D4", "H6");
+
+        assertEquals("Is straight", "straight", category.getName());
+    }
+}
