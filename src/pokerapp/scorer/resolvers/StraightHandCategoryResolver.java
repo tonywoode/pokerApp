5e2ba3300b85b999effd31a989dcaf71e7bf031a
ar1g <ari.lounging+GH@gmail.com>
2013-02-28 16:49:25 +0000

    StraightHandCategoryResolver: simplified Straight evaluation

--- src/pokerapp/scorer/resolvers/StraightHandCategoryResolver.java
+++ src/pokerapp/scorer/resolvers/StraightHandCategoryResolver.java
@@ -2,6 +2,8 @@ package pokerapp.scorer.resolvers;
 
 import pokerapp.Hand;
 import pokerapp.Card;
+import pokerapp.HandMatrix;
+import pokerapp.HandRankHistogram;
 import pokerapp.scorer.categories.StraightHandCategory;
 import pokerapp.utils.Constants;
 
@@ -16,9 +18,6 @@ import java.util.ArrayList;
  */
 public class StraightHandCategoryResolver extends HandCategoryResolver {
 
-
-
-
   public StraightHandCategoryResolver() {
     super(new StraightHandCategory("straight"));
   }
@@ -29,38 +28,18 @@ public class StraightHandCategoryResolver extends HandCategoryResolver {
   }
 
   private boolean isStraight(Hand hand) {
-    ArrayList<Card> sortedCards = hand.getSortedCards();
-
-    if (isStraight(sortedCards)) return true;
-
-    // handle the special case of Ace being low, iff we have an ace
-    Card maybeAce = sortedCards.get(0);
-    if (maybeAce.getRank() == Constants.MAX_RANKS) {
-      sortedCards.remove(0);
-      try {
-        sortedCards.add(new Card(maybeAce.getSuit(), Constants.RANK_ACE_LO));
-      } catch (Exception e) {
-        // TODO: fix this; checked exceptions are horrific :)
-      }
-      if (isStraight(sortedCards)) return true;
-    }
-
-    return false;
+      HandRankHistogram handRankHistogram = new HandRankHistogram(hand);
+    if (isStraight(handRankHistogram))
+          return true;
+    else {
+          handRankHistogram.lowAcesOn();
+          boolean retval = (isStraight(handRankHistogram));
+          handRankHistogram.lowAcesOff();
+          return  retval;
+       }
   }
 
-  private boolean isStraight(ArrayList<Card> sortedCards) {
-    Card prevCard = sortedCards.get(0);
-    int sequenceLength = 1;
-
-    for (int iter = 1; iter < sortedCards.size(); ++iter) {
-      Card currentCard = sortedCards.get(iter);
-
-      if (currentCard.getRank() == prevCard.getRank() + 1)
-        if (++sequenceLength == Constants.FULL_DECK)
-          return true;
-
-      prevCard = currentCard;
-    }
-    return false;
+  private boolean isStraight(HandRankHistogram handRankHistogram) {
+      return(handRankHistogram.toString().contains("11111"));
   }
 }
