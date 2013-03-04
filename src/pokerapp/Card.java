f6f0ed98c5e085aed4f9cd143aa71e751a031ce9
ar1g <ari.lounging+GH@gmail.com>
2013-03-04 12:44:01 +0000

    Card: getOrdinal should return the card number - changed method and javadoc

--- src/pokerapp/Card.java
+++ src/pokerapp/Card.java
@@ -18,7 +18,7 @@ public class Card {
       throw new IllegalArgumentException("Suit must not be null");
 
     // supports aces high & low; TODO: is this sensible?
-    if (rank < Constants.RANK_ACE_LO && rank > Constants.MAX_RANKS)
+    if (rank < Constants.RANK_ACE_LO && rank > Constants.NUM_RANKS)
       throw new IllegalArgumentException("Illegal Rank: " + rank);
 
     this.suit = suit;
@@ -30,7 +30,13 @@ public class Card {
    * @return the card's ordinal
    */
   //TODO: what is that exactly?
-  public int getOrdinal() { return suit.getNumber() * getRank(); }
+  // Ari: I think this is a hangover from card number when we were going to
+  // number the deck from 1 to 52
+  // in which case it should be
+  // suit.getNumber() * (getRank()-1)
+  // not suit.getNumber() * getRank(), so I've changed it to the former
+
+  public int getOrdinal() { return suit.getNumber() * (getRank()-1); }
 
   /**
    * returns the suit and rank of card
