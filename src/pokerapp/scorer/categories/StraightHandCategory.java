8bb1112a3067cb6481bdd07873cd31cbcfafab3b
ar1g <ari.lounging+GH@gmail.com>
2013-03-04 20:12:46 +0000

    StraightHandCategory: finished compareEqualCategories method.

--- src/pokerapp/scorer/categories/StraightHandCategory.java
+++ src/pokerapp/scorer/categories/StraightHandCategory.java
@@ -1,10 +1,8 @@
 package pokerapp.scorer.categories;
 
-import pokerapp.Hand;
-
 /**
  * Created with IntelliJ IDEA.
- * User: steve
+ * User: steve  + Ari
  * Date: 22/02/13
  * Time: 02:15
  * To change this template use File | Settings | File Templates.
@@ -14,18 +12,24 @@ public class StraightHandCategory extends HandCategory {
     super(name);
   }
 
+
+  //TODO:Steve, please write up this clone method
   @Override
   public StraightHandCategory clone() {
     return new StraightHandCategory(this.getName());
   }
 
+    /**
+     * highest top ranking card wins; on tie, try next highest card & so on
+     * @param rhs the hand passed in to compare against this
+     * @return 1 if host winds, 0 for tie, -1 if rhs wins
+     */
   @Override
   public int compareEqualCategories(HandCategory rhs) {
-    // highest top ranking card wins; on tie, try next highest card & so on
 
-    // TODO: really we need access to the sorted list of cards from the category resolver...
+    int rhs_score = Integer.parseInt (rhs.getRankHistogram().toString());
+    int lhs_score = Integer.parseInt (this.getRankHistogram().toString());
 
-
-    return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    return(Integer.compare(lhs_score, rhs_score));
   }
 }
