a421a903a9769527ef8c23800c8e1f89bd61e309
Tony Woode <tonywoodecode@gmail.com>
2013-03-09 21:35:41 +0000

    rank again

--- src/pokerapp/Rank.java
+++ src/pokerapp/Rank.java
@@ -1,10 +1,20 @@
 package pokerapp;
 
+/**
+ * Describes ranks
+ * @author Steve, Tony
+ *
+ */
 public enum Rank {  
     DEUCE(2),
     THREE(3),
     FOUR(4),
     FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
     JACK(2,'J'),  
     QUEEN(12, 'Q'),
     KING(13,'K'),
@@ -20,6 +30,6 @@ public enum Rank {
     }  
 
     Rank(int number) { 
-      this(number, (char)('0' + number));
+      this(number, (char)('0' + number)); //48 is ASCII zero
     } 
 }
