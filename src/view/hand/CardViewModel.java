4ef5c4e7a4c4cb35db7aadde82fd05e9a73d92dd
Steve Faulmann <stevefaulmann@gmail.com>
2013-03-09 22:30:42 +0000

    Basic code to display cards on screen. Uses a ViewModel. Presenter and View not separated yet.

---
+++ src/view/hand/CardViewModel.java
@@ -0,0 +1,73 @@
+package view.hand;
+
+import lombok.Getter;
+import lombok.Setter;
+import pokerapp.Card;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: steve
+ * Date: 09/03/13
+ * Time: 20:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class CardViewModel {
+
+  @Getter
+  @Setter
+  Card card;
+
+  @Getter
+  boolean isSelected;
+
+  @Getter
+  BufferedImage image; // how's this set?
+
+  public CardViewModel(Card card, BufferedImage image) {
+    this.card = card;
+    this.image = image;
+  }
+
+  public static CardViewModel create(Card card) {
+    String filename = filenameFromCard(card);
+
+    BufferedImage img;
+    try {
+      img = ImageIO.read(new File("pics/" + filename));
+    } catch (IOException e) {
+      System.out.println("Error: " + e.getMessage());
+      return null; // TODO: fix this...
+    }
+
+    return new CardViewModel(card, img);
+  }
+
+  private static String filenameFromCard(Card card) {
+    return ("" + getRankString(card.getRank()) + card.getSuit().getCode()).toLowerCase() + ".gif";
+  }
+
+  private static char getRankString(int rank) {
+    if (rank >= 0 && rank <= 10)
+      return (char) ('0' + rank);
+    else
+      switch (rank) {
+        case 11:
+          return 'J';
+        case 12:
+          return 'Q';
+        case 13:
+          return 'K';
+        case 14:
+          return 'A';
+        default:
+          throw new IllegalArgumentException("Illegal rank");
+      }
+
+  }
+
+}
---
+++ src/view/hand/ImageView.java
@@ -0,0 +1,41 @@
+package view.hand;
+
+import lombok.Getter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: steve
+ * Date: 09/03/13
+ * Time: 20:23
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImageView extends JComponent {
+
+  @Getter
+  private BufferedImage image;
+
+  public void setImage(BufferedImage img) {
+    this.image = img;
+    this.repaint();
+  }
+
+  @Override
+  public void paint(Graphics g) {
+    super.paint(g);
+    if (image != null)
+      g.drawImage(image, 0, 0, null);
+  }
+
+  @Override
+  public Dimension getPreferredSize() {
+    if (image == null) {
+      return new Dimension(100, 100);
+    } else {
+      return new Dimension(image.getWidth(null), image.getHeight(null));
+    }
+  }
+}
