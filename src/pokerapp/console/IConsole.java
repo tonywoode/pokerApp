fad41390f844d04a1cfbd2cb269794caeee4da0d
ar1g <ari.lounging+GH@gmail.com>
2013-03-16 19:55:58 +0000

    removed TODO from code, added to issue tracker

--- src/pokerapp/console/IConsole.java
+++ src/pokerapp/console/IConsole.java
@@ -1,15 +1,11 @@
 package pokerapp.console;
 
 /**
- * Created with IntelliJ IDEA.
  * @author steve
- * Date: 22/02/13
- * Time: 19:51
- * To change this template use File | Settings | File Templates.
+ * this class manages interaction to aid unit tests - a seam
+ * (Michael Feathers)
  */
 public interface IConsole {
-
-    //TODO: this is to manage interaction to aid unit tests - a seam (Michael Feathers)
 
   void writeMessage(String msg);
   void writeMessage(String msg, Object... args);
