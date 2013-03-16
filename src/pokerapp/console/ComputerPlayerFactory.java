d623bc4bdd95749b6e05b0d8c221e632ce07639e
ar1g <ari.lounging+GH@gmail.com>
2013-03-16 19:51:13 +0000

    code clean - magic numbers removed

--- src/pokerapp/console/ComputerPlayerFactory.java
+++ src/pokerapp/console/ComputerPlayerFactory.java
@@ -6,17 +6,21 @@ package pokerapp.console;
  */
 class ComputerPlayerFactory {
 
+  public final int HARD_COMPUTER = 3;
+  public final int STANDARD_COMPUTER = 2;
+  public final int EASY_COMPUTER = 1;
+
   public ComputerPlayer makeComputerPlayer(String playerName, int playerDifficulty) {
 
     switch (playerDifficulty) {
 
-      case 3:
+      case HARD_COMPUTER:
         return new HardComputerPlayer(playerName);
 
-      case 2:
+      case STANDARD_COMPUTER:
         return new StandardComputerPlayer(playerName);
 
-      case 1:
+      case EASY_COMPUTER:
         return new EasyComputerPlayer(playerName);
 
     }
