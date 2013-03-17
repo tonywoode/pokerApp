03cd5a8ef22ee07fd5a8bd49e7404663bdfca0fc
Steve Faulmann <stevefaulmann@gmail.com>
2013-03-17 11:41:53 +0000

    Added an import for ImageView. Build broken otherwise.

--- src/view/hand/FilteredHandRunner.java
+++ src/view/hand/FilteredHandRunner.java
@@ -5,6 +5,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pokerapp.Card;
 import pokerapp.Suit;
+import view.ImageView;
+import view.imagefilters.IImageFilter;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,10 +14,7 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
-import java.util.*;
-
-import com.jhlabs.image.*;
-import view.imagefilters.IImageFilter;
+import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
@@ -27,26 +26,26 @@ import view.imagefilters.IImageFilter;
 
 public class FilteredHandRunner {
 
-    private IImageFilter selectedImageFilter;
-    private java.util.List<CardViewModel> cardViewModelList = new ArrayList<CardViewModel>();
+  private IImageFilter selectedImageFilter;
+  private java.util.List<CardViewModel> cardViewModelList = new ArrayList<CardViewModel>();
 
-    public FilteredHandRunner(IImageFilter filter) {
-        this.selectedImageFilter = filter;
-    }
+  public FilteredHandRunner(IImageFilter filter) {
+    this.selectedImageFilter = filter;
+  }
 
   public static void main(String[] args) {
-      try {
-          ApplicationContext appContext = new ClassPathXmlApplicationContext("/gui-game-application-context.xml");
-          //gets the bean, cast it to ConsoleGame, a Spring managed object
-          FilteredHandRunner game = (FilteredHandRunner)appContext.getBean("filteredHandRunner");
+    try {
+      ApplicationContext appContext = new ClassPathXmlApplicationContext("/gui-game-application-context.xml");
+      //gets the bean, cast it to ConsoleGame, a Spring managed object
+      FilteredHandRunner game = (FilteredHandRunner) appContext.getBean("filteredHandRunner");
 
-          game.run();
+      game.run();
 
-      } catch (BeansException e) {
-          System.out.println("Error:" + e.getMessage());
-      }
+    } catch (BeansException e) {
+      System.out.println("Error:" + e.getMessage());
+    }
 
-      System.out.println("Exiting...");
+    System.out.println("Exiting...");
   }
 
   public void run() {
@@ -72,55 +71,55 @@ public class FilteredHandRunner {
       BufferedImage img = cardVM.getImage();
 
       if (iter % 2 == 0) {
-          BufferedImage filteredImg;
-          filteredImg = selectedImageFilter.filter(cloneImage(img));
-          cardVM.setSelected(true);
-          cardVM.setSelectedImage(filteredImg);
-          img = filteredImg;
+        BufferedImage filteredImg;
+        filteredImg = selectedImageFilter.filter(cloneImage(img));
+        cardVM.setSelected(true);
+        cardVM.setSelectedImage(filteredImg);
+        img = filteredImg;
       }
 
-      ImageView imageView = new ImageView(iter-2);
+      ImageView imageView = new ImageView(iter - 2);
 
       imageView.addMouseListener(new MouseListener() {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-            ImageView iv = (ImageView)e.getSource();
+          ImageView iv = (ImageView) e.getSource();
 
-            CardViewModel cardVM = cardViewModelList.get(iv.getIndex());
+          CardViewModel cardVM = cardViewModelList.get(iv.getIndex());
 
-            cardVM.setSelected(!cardVM.isSelected());
+          cardVM.setSelected(!cardVM.isSelected());
 
-            if (cardVM.getSelectedImage() == null) {
-                BufferedImage selImg = cloneImage(cardVM.getUnselectedImage());
-                cardVM.setSelectedImage(selectedImageFilter.filter(selImg));
-            }
+          if (cardVM.getSelectedImage() == null) {
+            BufferedImage selImg = cloneImage(cardVM.getUnselectedImage());
+            cardVM.setSelectedImage(selectedImageFilter.filter(selImg));
+          }
 
-            BufferedImage img = cardVM.getImage();
+          BufferedImage img = cardVM.getImage();
 
-            iv.setImage(img);
+          iv.setImage(img);
 
         }
 
-          @Override
-          public void mousePressed(MouseEvent e) {
-              //To change body of implemented methods use File | Settings | File Templates.
-          }
+        @Override
+        public void mousePressed(MouseEvent e) {
+          //To change body of implemented methods use File | Settings | File Templates.
+        }
 
-          @Override
-          public void mouseReleased(MouseEvent e) {
-              //To change body of implemented methods use File | Settings | File Templates.
-          }
+        @Override
+        public void mouseReleased(MouseEvent e) {
+          //To change body of implemented methods use File | Settings | File Templates.
+        }
 
-          @Override
-          public void mouseEntered(MouseEvent e) {
-              //To change body of implemented methods use File | Settings | File Templates.
-          }
+        @Override
+        public void mouseEntered(MouseEvent e) {
+          //To change body of implemented methods use File | Settings | File Templates.
+        }
 
-          @Override
-          public void mouseExited(MouseEvent e) {
-              //To change body of implemented methods use File | Settings | File Templates.
-          }
+        @Override
+        public void mouseExited(MouseEvent e) {
+          //To change body of implemented methods use File | Settings | File Templates.
+        }
       });
 
       imageView.setImage(img);
@@ -145,17 +144,17 @@ public class FilteredHandRunner {
           CardViewModel cardVM = CardViewModel.create(card);
           cardViewModelList.add(cardVM);
 
-            BufferedImage img = cardVM.getImage();
+          BufferedImage img = cardVM.getImage();
 
-            if (iter % 2 == 0) {
-                BufferedImage filteredImg;
-                filteredImg = selectedImageFilter.filter(cloneImage(img));
-                cardVM.setSelected(true);
-                cardVM.setSelectedImage(filteredImg);
-                img = filteredImg;
-            }
+          if (iter % 2 == 0) {
+            BufferedImage filteredImg;
+            filteredImg = selectedImageFilter.filter(cloneImage(img));
+            cardVM.setSelected(true);
+            cardVM.setSelectedImage(filteredImg);
+            img = filteredImg;
+          }
 
-            imageViews.get(iter - 2).setImage(img);
+          imageViews.get(iter - 2).setImage(img);
         }
       }
     });
@@ -168,11 +167,11 @@ public class FilteredHandRunner {
     frame.setVisible(true);
   }
 
-    BufferedImage cloneImage(BufferedImage img) {
-        ColorModel cm = img.getColorModel();
-        boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
-        WritableRaster raster = img.copyData(null);
-        return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
-    }
+  BufferedImage cloneImage(BufferedImage img) {
+    ColorModel cm = img.getColorModel();
+    boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
+    WritableRaster raster = img.copyData(null);
+    return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
+  }
 
 }
