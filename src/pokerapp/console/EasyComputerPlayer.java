e81ff578cc55982d55a437a3b2121c7d2d9177a3
ar1g <ari.lounging+GH@gmail.com>
2013-03-21 02:52:25 +0000

    added TODOs for magic numbers

--- src/pokerapp/console/EasyComputerPlayer.java
+++ src/pokerapp/console/EasyComputerPlayer.java
@@ -26,7 +26,7 @@ public class EasyComputerPlayer extends ComputerPlayer {
   @Override
   protected String exchangeDecision(StringBuilder commandBuilder, String handType) {
 
-    Random generator = new Random(19580427);
+    Random generator = new Random(19580427); //TODO:magic number?
 
     Rank r = Rank.from(generator.nextInt(Constants.NUM_RANKS));
 
@@ -35,7 +35,7 @@ public class EasyComputerPlayer extends ComputerPlayer {
     int n = 0;
     for (Card card : getHand()) {
       n++;
-      if (card.getRank().beats(targetRank) && commandBuilder.length() < 6)
+      if (card.getRank().beats(targetRank) && commandBuilder.length() < 6)  //TODO:magic number, no longer 6
         commandBuilder.append(n).append(" ");
     }
     return commandBuilder.toString();
