fac942e9c58d457e18bb7bf163a9a16b4478a58a
ar1g <ari.lounging+GH@gmail.com>
2013-03-31 22:40:07 +0100

    shorter turn dialogue

--- src/pokerapp/console/turns/ComputerPlayerTurn.java
+++ src/pokerapp/console/turns/ComputerPlayerTurn.java
@@ -3,11 +3,8 @@ package pokerapp.console.turns;
 import lombok.AllArgsConstructor;
 import pokerapp.ComputerPlayer;
 import pokerapp.Dealer;
-import pokerapp.Deck;
-import pokerapp.Player;
 import pokerapp.console.Console;
 import pokerapp.console.ExchangeSetting;
-import pokerapp.scorer.HandScorerBuilder;
 import pokerapp.scorer.PokerGameEvaluator;
 import pokerapp.scorer.scoredhands.ScoredHand;
 
@@ -28,7 +25,7 @@ public class ComputerPlayerTurn implements ConsolePlayerTurn {
   
   @Override
   public void playTurn(Dealer dealer, Console console) {
-    console.writeMessage("Player {0}'s turn", player.getPlayerName());
+    console.writeMessage("{0} plays", player.getPlayerName());
 
     ScoredHand scoredHand = pokerGameEvaluator.score(player.getHand());
 
